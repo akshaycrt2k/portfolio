76170cdf7b18b048dafd51d29acc71ba2ff8a0a6
Akshay Mundotia <akshaycrt2k@gmail.com>
2022-09-28 23:05:51 +0530

    Added CD for prod and staging with Actions

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+Color backgroundHomeColor = Colors.black87;
+Color titleTextColor = Colors.white;
+Color bodyTextColor = Colors.cyan.shade50;
--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/constants.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Akshay Mundotia"),
+    return Container(
+      color: backgroundHomeColor,
+      child: Column(
+
+        children: [
+          Container(
+              margin: const EdgeInsets.all(16.0),
+              child: Text("Akshay Mundotia",
+                  style: TextStyle(color: titleTextColor))),
+          Container(
+              margin: const EdgeInsets.all(16.0),
+              child:
+                  Text("Coming Soon...", style: TextStyle(fontSize: 12.0, color: bodyTextColor)))
+        ],
+      ),
     );
   }
 }
